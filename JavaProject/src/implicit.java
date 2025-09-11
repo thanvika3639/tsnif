c9b7337a2e87278c6a4b03e7a7f415534b0c5c31
thanvika3639 <emp714506@amentcapital.com>
2025-09-11 22:24:41 +0530

    First program

---
+++ JavaProject/src/implicit.java
@@ -0,0 +1,28 @@
+public class implicit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		float b = a;
+		System.out.println("Value of a :"   +a);
+		System.out.println("Value of b :"   +b);
+		
+		char c = 'C';
+		int d = c;
+		System.out.println("Value of c :"   +c);
+		System.out.println("Value of d :"   +d);
+		
+		byte e = 100 ;
+		double f = e ;
+		System.out.println("Value of e :"   +e);
+		System.out.println("Value of f :"   +f);
+		
+		char g = 'A';
+		float h = g ;
+		System.out.println("Value of g :"   +g);
+		System.out.println("Value of h :"   +h);
+		
+		long i = 234355745;
+	}
+
+}
