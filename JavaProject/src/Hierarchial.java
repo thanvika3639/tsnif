6e1d936065db6f9e4985b0270342f09a4cd88bda
thanvika3639 <emp714506@amentcapital.com>
2025-09-17 13:23:06 +0530

    Hierarchial inheritance

--- JavaProject/src/Hierarchial.java
+++ JavaProject/src/Hierarchial.java
@@ -4,41 +4,37 @@
  }
 }
 
-//Child class 1
 class SportsCar extends Car3 {
  void turbo() {
      System.out.println("SportsCar has turbo speed.");
  }
 }
 
-//Child class 2
 class ElectricCar extends Car3 {
  void charge() {
      System.out.println("ElectricCar charges with electricity.");
  }
 }
 
-//Child class 3
 class LuxuryCar extends Car3 {
  void comfort() {
      System.out.println("LuxuryCar has high comfort features.");
  }
 }
 
-//Main class
 public class Hierarchial {
  public static void main(String[] args) {
      
      SportsCar sc = new SportsCar();
-     sc.start();   // from Car
-     sc.turbo();   // own method
+     sc.start();   
+     sc.turbo();  
 
      ElectricCar ec = new ElectricCar();
-     ec.start();   // from Car
-     ec.charge();  // own method
+     ec.start();   
+     ec.charge();  
 
      LuxuryCar lc = new LuxuryCar();
-     lc.start();   // from Car
-     lc.comfort(); // own method
+     lc.start();   
+     lc.comfort(); 
  }
 }
